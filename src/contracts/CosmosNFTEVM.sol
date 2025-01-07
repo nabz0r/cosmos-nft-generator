144b182549901d357412190d1ad10c856a6ef2be
nabilux <nabz0r@gmail.com>
2025-01-07 10:32:11 +0100

    feat: add emergency pause mechanism

--- src/contracts/CosmosNFTEVM.sol
+++ src/contracts/CosmosNFTEVM.sol
@@ -4,39 +4,38 @@ pragma solidity ^0.8.19;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract CosmosNFTEVM is ERC721URIStorage, ERC721Enumerable, ReentrancyGuard, Ownable, VRFConsumerBase {
+contract CosmosNFTEVM is ERC721URIStorage, ERC721Enumerable, ReentrancyGuard, Ownable, VRFConsumerBase, Pausable {
     using Strings for uint256;
 
-    // Prix fixe en ETH
     uint256 public constant MINT_PRICE = 0.1 ether;
     uint256 public constant MAX_SUPPLY = 10000;
     uint256 public constant TEAM_RESERVE = 500;
     uint256 public constant COLLAB_RESERVE = 100;
 
-    // Chainlink VRF pour la génération aléatoire
     bytes32 private keyHash;
     uint256 private fee;
     
-    // Mappings
     mapping(uint256 => PlanetAttributes) public planetAttributes;
     mapping(address => bool) public isCollaborator;
     mapping(uint256 => bool) public isCollabNFT;
     mapping(bytes32 => uint256) private requestToTokenId;
     
     struct PlanetAttributes {
-        uint8 planetType;    // 0-3 (Rocky, Ice, Gas, Forest)
-        uint8 rarity;        // 0-4 (Common to Legendary)
+        uint8 planetType;
+        uint8 rarity;
         string specialTrait;
     }
     
-    // Events
     event MintRequested(address indexed to, bytes32 requestId);
     event PlanetRevealed(uint256 indexed tokenId, uint8 planetType, uint8 rarity);
+    event EmergencyPause(address indexed trigger);
+    event EmergencyUnpause(address indexed trigger);
     
     constructor(
         address _vrfCoordinator,
@@ -47,14 +46,13 @@ contract CosmosNFTEVM is ERC721URIStorage, ERC721Enumerable, ReentrancyGuard, Ow
         VRFConsumerBase(_vrfCoordinator, _linkToken) 
     {
         keyHash = _keyHash;
-        fee = 0.1 * 10**18; // 0.1 LINK
+        fee = 0.1 * 10**18;
     }
 
-    function mint() public payable nonReentrant {
+    function mint() public payable nonReentrant whenNotPaused {
         require(msg.value >= MINT_PRICE, "Insufficient payment");
         require(totalSupply() < MAX_SUPPLY - TEAM_RESERVE - COLLAB_RESERVE, "Max supply reached");
         
-        // Demande de nombre aléatoire à Chainlink VRF
         bytes32 requestId = requestRandomness(keyHash, fee);
         uint256 tokenId = totalSupply();
         requestToTokenId[requestId] = tokenId;
@@ -63,10 +61,9 @@ contract CosmosNFTEVM is ERC721URIStorage, ERC721Enumerable, ReentrancyGuard, Ow
         emit MintRequested(msg.sender, requestId);
     }
     
-    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override {
+    function fulfillRandomness(bytes32 requestId, uint256 randomness) internal override whenNotPaused {
         uint256 tokenId = requestToTokenId[requestId];
         
-        // Génération des attributs basée sur le nombre aléatoire
         uint8 planetType = uint8(randomness % 4);
         uint8 rarity = _calculateRarity(uint8((randomness >> 8) % 100));
         
@@ -79,30 +76,39 @@ contract CosmosNFTEVM is ERC721URIStorage, ERC721Enumerable, ReentrancyGuard, Ow
         emit PlanetRevealed(tokenId, planetType, rarity);
     }
     
-    function mintCollabNFT(address to, string memory specialTrait) public {
+    function mintCollabNFT(address to, string memory specialTrait) public whenNotPaused {
         require(isCollaborator[msg.sender], "Not a collaborator");
         require(totalSupply() < COLLAB_RESERVE, "Collab reserve depleted");
         
         uint256 tokenId = totalSupply();
         planetAttributes[tokenId] = PlanetAttributes({
             planetType: uint8(block.timestamp % 4),
-            rarity: 4, // Legendary
+            rarity: 4,
             specialTrait: specialTrait
         });
         
         isCollabNFT[tokenId] = true;
         _safeMint(to, tokenId);
     }
-    
-    function _calculateRarity(uint8 rand) internal pure returns (uint8) {
-        if (rand < 50) return 0;      // 50% Common
-        if (rand < 75) return 1;      // 25% Uncommon
-        if (rand < 90) return 2;      // 15% Rare
-        if (rand < 98) return 3;      // 8% Epic
-        return 4;                     // 2% Legendary
+
+    function pause() public onlyOwner {
+        _pause();
+        emit EmergencyPause(msg.sender);
     }
 
-    // Fonction pour générer les métadonnées on-chain
+    function unpause() public onlyOwner {
+        _unpause();
+        emit EmergencyUnpause(msg.sender);
+    }
+    
+    function _calculateRarity(uint8 rand) internal pure returns (uint8) {
+        if (rand < 50) return 0;
+        if (rand < 75) return 1;
+        if (rand < 90) return 2;
+        if (rand < 98) return 3;
+        return 4;
+    }
+
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         PlanetAttributes memory attrs = planetAttributes[tokenId];
         
@@ -132,6 +138,7 @@ contract CosmosNFTEVM is ERC721URIStorage, ERC721Enumerable, ReentrancyGuard, Ow
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
         override(ERC721, ERC721Enumerable)
+        whenNotPaused
     {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
